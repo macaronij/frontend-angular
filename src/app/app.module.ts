0cf72afef71111d1a68a353fcd4fb408f23e624c
macaronij <macaronij@gmail.com>
2022-09-22 17:29:14 -0300

    cambiada las imagenes por botones en la botonera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,6 +14,7 @@ import { Pagina404Component } from './pagina404/pagina404.component';
 import { BotoneraComponent } from './botonera/botonera.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,11 +27,13 @@ import { ProyectosComponent } from './proyectos/proyectos.component';
     Pagina404Component,
     BotoneraComponent,
     ContactoComponent,
-    ProyectosComponent
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators } from "@angular/forms";
+
 
 @Component({
   selector: 'app-contacto',
@@ -7,12 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
 
   ngOnInit(): void {
   }
   
-  enviando() {
-    alert("NO IMPLEMENTADO AUN")
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      nombre: ["",[Validators.required, Validators.minLength(5), Validators.maxLength(12)]],
+      correo:["", [Validators.email, Validators.required] ],
+      mensaje:["",[Validators.required, Validators.minLength(8)]]
+    })
   }
+
+
+  get Nombre(){
+    return this.form.get("nombre");
+  }
+  get Correo(){
+    return this.form.get("correo");
+  }
+  get Mensaje(){
+    return this.form.get("mensaje");
+  }
+
+  enviando(event: Event) {
+    console.log(this.form.value.nombre)
+    if (this.Nombre) {
+      console.log(this.Nombre.errors)
+    }
+  }
+  
+
+
+
 }
